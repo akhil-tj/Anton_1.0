f3bc4d96083ffb32aa6bcb9041ee47852ab18a9f
itexpert2572000 <itexpert2572000@gmail.com>
2021-05-27 11:15:04 +0530

    updated drawer layout

--- lib/Components/navigation_drawer.dart
+++ lib/Components/navigation_drawer.dart
@@ -55,7 +55,7 @@ class NavigationDrawer extends StatelessWidget {
               ),
               child: Row(
                 mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Image(
                     image: AssetImage(
@@ -63,34 +63,22 @@ class NavigationDrawer extends StatelessWidget {
                     height: 40,
                     width: 40,
                   ),
-                  // SizedBox(
-                  //   height: 16,
-                  // ),
                   Image(
                     image: AssetImage('images/ant-design_github-filled.png'),
                     height: 40,
                     width: 40,
                   ),
-                  // SizedBox(
-                  //   height: 16,
-                  // ),
                   Image(
                     image: AssetImage(
                         'images/entypo-social_linkedin-with-circle.png'),
                     height: 40,
                     width: 40,
                   ),
-                  // SizedBox(
-                  //   height: 16,
-                  // ),
                   Image(
                     image: AssetImage('images/ant-design_instagram-filled.png'),
                     height: 40,
                     width: 40,
                   ),
-                  // SizedBox(
-                  //   height: 16,
-                  // ),
                   Image(
                     image: AssetImage(
                         'images/ant-design_behance-circle-filled.png'),
@@ -129,127 +117,3 @@ class NavigationDrawer extends StatelessWidget {
     );
   }
 }
-
-// ListView(
-//           children: [
-//             Container(
-//               padding: EdgeInsets.symmetric(
-//                 vertical: 8,
-//                 horizontal: 24,
-//               ),
-//               child: Text(
-//                 'Home',
-//                 style: landingMobileTxt2,
-//               ),
-//             ),
-//             Container(
-//               padding: EdgeInsets.symmetric(
-//                 vertical: 8,
-//                 horizontal: 24,
-//               ),
-//               child: Text(
-//                 'Skills',
-//                 style: landingMobileTxt1,
-//               ),
-//             ),
-//             Container(
-//               padding: EdgeInsets.symmetric(
-//                 vertical: 8,
-//                 horizontal: 24,
-//               ),
-//               child: Text(
-//                 'Works',
-//                 style: landingMobileTxt1,
-//               ),
-//             ),
-//             Container(
-//               padding: EdgeInsets.symmetric(
-//                 vertical: 8,
-//                 horizontal: 24,
-//               ),
-//               child: Text(
-//                 'Contact',
-//                 style: landingMobileTxt1,
-//               ),
-//             ),
-//             Container(
-//               color: Colors.amber,
-//               padding: EdgeInsets.symmetric(
-//                 vertical: 14,
-//                 horizontal: 16,
-//               ),
-//               child: Row(
-//                 mainAxisSize: MainAxisSize.max,
-//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                 children: [
-//                   Image(
-//                     image: AssetImage(
-//                         'images/ant-design_twitter-circle-filled.png'),
-//                     height: 40,
-//                     width: 40,
-//                   ),
-//                   // SizedBox(
-//                   //   height: 16,
-//                   // ),
-//                   Image(
-//                     image:
-//                         AssetImage('images/ant-design_github-filled.png'),
-//                     height: 40,
-//                     width: 40,
-//                   ),
-//                   // SizedBox(
-//                   //   height: 16,
-//                   // ),
-//                   Image(
-//                     image: AssetImage(
-//                         'images/entypo-social_linkedin-with-circle.png'),
-//                     height: 40,
-//                     width: 40,
-//                   ),
-//                   // SizedBox(
-//                   //   height: 16,
-//                   // ),
-//                   Image(
-//                     image: AssetImage(
-//                         'images/ant-design_instagram-filled.png'),
-//                     height: 40,
-//                     width: 40,
-//                   ),
-//                   // SizedBox(
-//                   //   height: 16,
-//                   // ),
-//                   Image(
-//                     image: AssetImage(
-//                         'images/ant-design_behance-circle-filled.png'),
-//                     height: 40,
-//                     width: 40,
-//                   ),
-//                 ],
-//               ),
-//             ),
-//             Padding(
-//               padding: const EdgeInsets.symmetric(
-//                 horizontal: 24,
-//                 vertical: 4,
-//               ),
-//               child: Row(
-//                 mainAxisSize: MainAxisSize.min,
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 children: [
-//                   Text(
-//                     'Made with ',
-//                     style: navbarTabletBtnTxt,
-//                   ),
-//                   Icon(
-//                     Icons.favorite,
-//                     color: Colors.red,
-//                   ),
-//                   Text(
-//                     ' Akhil T J',
-//                     style: navbarTabletBtnTxt,
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ],
-//         ),
