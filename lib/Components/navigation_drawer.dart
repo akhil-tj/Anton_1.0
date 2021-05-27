c7d9b095576ddc945b6383be433d98afb20bcf04
itexpert2572000 <itexpert2572000@gmail.com>
2021-05-27 15:38:30 +0530

    solved drawer issues

--- lib/Components/navigation_drawer.dart
+++ lib/Components/navigation_drawer.dart
@@ -6,113 +6,126 @@ class NavigationDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Container(
-        padding: EdgeInsets.fromLTRB(24, 40, 24, 16),
-        color: navyColor,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 8,
-              ),
-              child: Text(
-                'Home',
-                style: landingMobileTxt2,
+      child: LayoutBuilder(
+        builder: (context, constraint) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraint.maxHeight),
+              child: IntrinsicHeight(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(24, 40, 24, 16),
+                  color: navyColor,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 8,
+                        ),
+                        child: Text(
+                          'Home',
+                          style: landingMobileTxt2,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 8,
+                        ),
+                        child: Text(
+                          'Skills',
+                          style: landingMobileTxt1,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 8,
+                        ),
+                        child: Text(
+                          'Works',
+                          style: landingMobileTxt1,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 8,
+                        ),
+                        child: Text(
+                          'Contact',
+                          style: landingMobileTxt1,
+                        ),
+                      ),
+                      Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 8,
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Image(
+                              image: AssetImage(
+                                  'images/ant-design_twitter-circle-filled.png'),
+                              height: 40,
+                              width: 40,
+                            ),
+                            Image(
+                              image: AssetImage(
+                                  'images/ant-design_github-filled.png'),
+                              height: 40,
+                              width: 40,
+                            ),
+                            Image(
+                              image: AssetImage(
+                                  'images/entypo-social_linkedin-with-circle.png'),
+                              height: 40,
+                              width: 40,
+                            ),
+                            Image(
+                              image: AssetImage(
+                                  'images/ant-design_instagram-filled.png'),
+                              height: 40,
+                              width: 40,
+                            ),
+                            Image(
+                              image: AssetImage(
+                                  'images/ant-design_behance-circle-filled.png'),
+                              height: 40,
+                              width: 40,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 8,
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Made with ',
+                              style: navbarTabletBtnTxt,
+                            ),
+                            Icon(
+                              Icons.favorite,
+                              color: Colors.red,
+                            ),
+                            Text(
+                              ' Akhil T J',
+                              style: navbarTabletBtnTxt,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 8,
-              ),
-              child: Text(
-                'Skills',
-                style: landingMobileTxt1,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 8,
-              ),
-              child: Text(
-                'Works',
-                style: landingMobileTxt1,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 8,
-              ),
-              child: Text(
-                'Contact',
-                style: landingMobileTxt1,
-              ),
-            ),
-            Spacer(),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 8,
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Image(
-                    image: AssetImage(
-                        'images/ant-design_twitter-circle-filled.png'),
-                    height: 40,
-                    width: 40,
-                  ),
-                  Image(
-                    image: AssetImage('images/ant-design_github-filled.png'),
-                    height: 40,
-                    width: 40,
-                  ),
-                  Image(
-                    image: AssetImage(
-                        'images/entypo-social_linkedin-with-circle.png'),
-                    height: 40,
-                    width: 40,
-                  ),
-                  Image(
-                    image: AssetImage('images/ant-design_instagram-filled.png'),
-                    height: 40,
-                    width: 40,
-                  ),
-                  Image(
-                    image: AssetImage(
-                        'images/ant-design_behance-circle-filled.png'),
-                    height: 40,
-                    width: 40,
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 8,
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Made with ',
-                    style: navbarTabletBtnTxt,
-                  ),
-                  Icon(
-                    Icons.favorite,
-                    color: Colors.red,
-                  ),
-                  Text(
-                    ' Akhil T J',
-                    style: navbarTabletBtnTxt,
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
