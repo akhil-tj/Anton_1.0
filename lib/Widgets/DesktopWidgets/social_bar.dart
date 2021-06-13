5af67607eeb50bb78a89f23d6edf004831994754
akhil-tj <itexpert2572000@gmail.com>
2021-06-13 23:40:57 +0530

    loader

--- lib/Widgets/DesktopWidgets/social_bar.dart
+++ lib/Widgets/DesktopWidgets/social_bar.dart
@@ -1,3 +1,4 @@
+import 'package:anton_1_0/Style/color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -17,91 +18,106 @@ class SocialBar extends StatelessWidget {
           SizedBox(
             height: 24,
           ),
-          GestureDetector(
-            onTap: () async {
-              const url = 'https://twitter.com/akhil__tj';
-              if (await canLaunch(url)) {
-                await launch(url);
-              } else {
-                throw 'Could not launch $url';
-              }
-            },
-            child: SvgPicture.asset(
-              'images/twitter.svg',
-              height: 40,
-              width: 40,
+          MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: GestureDetector(
+              onTap: () async {
+                const url = 'https://twitter.com/akhil__tj';
+                if (await canLaunch(url)) {
+                  await launch(url);
+                } else {
+                  throw 'Could not launch $url';
+                }
+              },
+              child: SvgPicture.asset(
+                'images/twitter.svg',
+                height: 40,
+                width: 40,
+              ),
             ),
           ),
           SizedBox(
             height: 16,
           ),
-          GestureDetector(
-            onTap: () async {
-              const url = 'https://github.com/akhil-tj';
-              if (await canLaunch(url)) {
-                await launch(url);
-              } else {
-                throw 'Could not launch $url';
-              }
-            },
-            child: SvgPicture.asset(
-              'images/github.svg',
-              height: 40,
-              width: 40,
+          MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: GestureDetector(
+              onTap: () async {
+                const url = 'https://github.com/akhil-tj';
+                if (await canLaunch(url)) {
+                  await launch(url);
+                } else {
+                  throw 'Could not launch $url';
+                }
+              },
+              child: SvgPicture.asset(
+                'images/github.svg',
+                height: 40,
+                width: 40,
+              ),
             ),
           ),
           SizedBox(
             height: 16,
           ),
-          GestureDetector(
-            onTap: () async {
-              const url = 'https://www.linkedin.com/in/akhiltj/';
-              if (await canLaunch(url)) {
-                await launch(url);
-              } else {
-                throw 'Could not launch $url';
-              }
-            },
-            child: SvgPicture.asset(
-              'images/linkedin.svg',
-              height: 40,
-              width: 40,
+          MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: GestureDetector(
+              onTap: () async {
+                const url = 'https://www.linkedin.com/in/akhiltj/';
+                if (await canLaunch(url)) {
+                  await launch(url);
+                } else {
+                  throw 'Could not launch $url';
+                }
+              },
+              child: SvgPicture.asset(
+                'images/linkedin.svg',
+                height: 40,
+                width: 40,
+              ),
             ),
           ),
           SizedBox(
             height: 16,
           ),
-          GestureDetector(
-            onTap: () async {
-              const url = 'https://www.instagram.com/akhil__tj/';
-              if (await canLaunch(url)) {
-                await launch(url);
-              } else {
-                throw 'Could not launch $url';
-              }
-            },
-            child: SvgPicture.asset(
-              'images/instagram.svg',
-              height: 40,
-              width: 40,
+          MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: GestureDetector(
+              onTap: () async {
+                const url = 'https://www.instagram.com/akhil__tj/';
+                if (await canLaunch(url)) {
+                  await launch(url);
+                } else {
+                  throw 'Could not launch $url';
+                }
+              },
+              child: SvgPicture.asset(
+                'images/instagram.svg',
+                height: 40,
+                width: 40,
+              ),
             ),
           ),
           SizedBox(
             height: 16,
           ),
-          GestureDetector(
-            onTap: () async {
-              const url = 'https://www.behance.net/TJ_DesignZ';
-              if (await canLaunch(url)) {
-                await launch(url);
-              } else {
-                throw 'Could not launch $url';
-              }
-            },
-            child: SvgPicture.asset(
-              'images/behance.svg',
-              height: 40,
-              width: 40,
+          MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: GestureDetector(
+              onTap: () async {
+                const url = 'https://www.behance.net/TJ_DesignZ';
+                if (await canLaunch(url)) {
+                  await launch(url);
+                } else {
+                  throw 'Could not launch $url';
+                }
+              },
+              child: SvgPicture.asset(
+                'images/behance.svg',
+                height: 40,
+                width: 40,
+              ),
             ),
           ),
           SizedBox(
